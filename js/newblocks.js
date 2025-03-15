6e38d11436e74d6730aa466f5e73977ad796437f
Aliaksei Karzhou <alexius90e@gmail.com>
2025-03-15 18:00:57 +0300

    feat: added available-programs

---
+++ js/newblocks.js
@@ -0,0 +1,28 @@
+const availableProgramsBlockEls = document.querySelectorAll('.available-programs__block');
+
+availableProgramsBlockEls.forEach((block) => {
+  const listItemsLimit = 12;
+  const listItems = block.querySelectorAll('.available-programs__list-item');
+  const list = block.querySelector('.available-programs__list');
+  const moreEl = block.querySelector('.available-programs__more');
+
+  if (listItems.length > listItemsLimit && moreEl) {
+    moreEl.classList.remove('hidden');
+  }
+
+  block.addEventListener('click', (event) => {
+    const isMoreButton = event.target.classList.contains('available-programs__more-button');
+
+    if (isMoreButton && list) {
+      const isActive = event.target.classList.contains('active');
+      event.target.innerText = `${isActive ? 'Показать' : 'Скрыть'} все направления`;
+      if (isActive) {
+        list.classList.remove('active');
+        event.target.classList.remove('active');
+      } else {
+        list.classList.add('active');
+        event.target.classList.add('active');
+      }
+    }
+  });
+});
